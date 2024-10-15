50b3112278641edcd42e25e7fce85d24fb68cb5b
NikX <41104633+NikXpro@users.noreply.github.com>
2024-10-15 16:52:09 +0200

    feat(index.js): enhance user card DOM structure to include profile link and additional details   Add a link to the photographer page with ID, and include the location, tagline, and price in the user card for better user experience.

--- scripts/templates/index.js
+++ scripts/templates/index.js
@@ -6,11 +6,30 @@ function indexTemplate(data) {
 
   function getUserCardDOM() {
     const article = document.createElement("article");
-    const img = createProfilePicture(portrait);
+
+    const link = document.createElement("a");
+    link.href = `photographer.html?id=${data.id}`;
+    article.appendChild(link);
+
+    const img = createProfilePicture(portrait, name);
+    link.appendChild(img);
+
     const h2 = document.createElement("h2");
     h2.textContent = name;
-    article.appendChild(img);
-    article.appendChild(h2);
+    link.appendChild(h2);
+
+    const location = document.createElement("p");
+    location.textContent = `${data.city}, ${data.country}`;
+    article.appendChild(location);
+
+    const tagline = document.createElement("p");
+    tagline.textContent = data.tagline;
+    article.appendChild(tagline);
+
+    const price = document.createElement("p");
+    price.textContent = `${data.price}€/jour`;
+    article.appendChild(price);
+
     return article;
   }
   return { name, getUserCardDOM };
