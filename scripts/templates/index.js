6c4883724b1a6c4900a10bf07f0e5cf26de84782
NikX <41104633+NikXpro@users.noreply.github.com>
2024-10-15 16:37:16 +0200

    refactor(index.js): simplify user card DOM creation by using createProfilePicture function and remove unnecessary picture variable   feat(index.js): export indexTemplate function for enhanced modularity and usability in other modules

--- scripts/templates/index.js
+++ scripts/templates/index.js
@@ -1,17 +1,20 @@
+// Importez la fonction depuis le fichier globalFunctions.js
+import { createProfilePicture } from "./globalFunctions.js";
+
 function indexTemplate(data) {
   const { name, portrait } = data;
 
-  const picture = `assets/photographers/${portrait}`;
-
   function getUserCardDOM() {
     const article = document.createElement("article");
-    const img = document.createElement("img");
-    img.setAttribute("src", picture);
+    const img = createProfilePicture(portrait);
     const h2 = document.createElement("h2");
     h2.textContent = name;
     article.appendChild(img);
     article.appendChild(h2);
     return article;
   }
-  return { name, picture, getUserCardDOM };
+  return { name, getUserCardDOM };
 }
+
+// Exportez la fonction indexTemplate
+export { indexTemplate };
